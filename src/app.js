a8bb08092d34612ac15c78726d75d518b0cc930d
aawoznia2644_do_github <ania.woznia@gmail.com_do_github>
2021-01-31 23:19:57 +0100

    services sections

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import './style.scss';
 import headerVideo from '../resources/background.mp4';
 import image1 from '../resources/electric-1080584_640.jpg';
+import image2 from '../resources/electrician-working.jpg';
+import image3 from '../resources/chandelier.png';
+import image4 from '../resources/socket.png';
+import image5 from '../resources/induction-stove.png';
+import image6 from '../resources/communications.png';
+
 
 const headerText = 'Andrzej Woźniak';
 const subHeaderText = 'Usługi elektryczne od'
@@ -17,3 +23,7 @@ h1.innerText = headerText;
 const subheader = document.createElement('span')
 subheader.innerText = subHeaderText + ' ' + yearsCounter + ' lat';
 h1.appendChild(subheader)
+
+const str = '. '
+document.getElementById("dotted1").innerHTML = str.repeat(627);
+document.getElementById("dotted2").innerHTML = str.repeat(627);
